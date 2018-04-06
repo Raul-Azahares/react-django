d68e8d7d3e4d3aa7dd7167806c9a1d6fb1b13191
raul-azahares <33665097+Raul-Azahares@users.noreply.github.com>
2018-04-06 14:25:14 +0000

    Update urls.py

--- project/codefights/urls.py
+++ project/codefights/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/book/', views.LeadListCreate.as_view()),
-    path('api/book/detail/$', views.BookDetail.as_view()),
+
 ]
